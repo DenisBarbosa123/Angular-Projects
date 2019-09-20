bc50568b14eddf775ad8a4e0067599e84096da3a
Denis Barbosa <denis_barbosa123@hotmail.com>
2019-09-19 21:59:46 -0300

    add trabalho sobre cadastro de veiculos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Veiculo } from './veiculo';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'trabalho1';
+
+  @ViewChild('myForm', {static : true})
+  meuForm : NgForm;
+
+  veiculo : Veiculo;
+  veiculos : Veiculo[];
+
+  ngOnInit(){
+    this.veiculo = new Veiculo();
+    this.veiculos = [];
+  }
+
+  salvarVeiculo(){
+    if(!this.veiculo.id){
+      this.veiculo.id = (new Date()).getTime();
+      this.veiculos.push(this.veiculo);
+    }else{
+      console.log("editando...")
+      let oldVeiculo = this.veiculos.find(v => v.id == this.veiculo.id);
+      oldVeiculo.id = this.veiculo.id;
+      oldVeiculo.marca = this.veiculo.marca;
+      oldVeiculo.modelo = this.veiculo.modelo;
+      oldVeiculo.tipo = this.veiculo.tipo;
+      oldVeiculo.cor = this.veiculo.cor;
+      oldVeiculo.ano = this.veiculo.ano;
+    }
+
+    this.veiculo = new Veiculo();
+    this.meuForm.reset();
+  }
+
+  editarVeiculo(veiculo : Veiculo){
+    this.veiculo = new Veiculo(veiculo.id , veiculo.tipo, 
+      veiculo.marca, veiculo.modelo, veiculo.cor, veiculo.ano);
+  }
+
+  removerVeiculo(veiculo : Veiculo){
+    let index = this.veiculos.findIndex(v => v.id == veiculo.id);
+    this.veiculos.splice(index, 1);
+  }
 }
